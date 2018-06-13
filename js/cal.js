f8e534cf4b88058713a4ee3fd4ca8f0d9f4495a8
Mailloux <t_mailloux@tss-hr420-dm40.fc.ca>
2018-06-13 14:00:36 -0400

    added html and js

--- js/cal.js
+++ js/cal.js
@@ -3,4 +3,26 @@
 (function() {
 	console.log("JS file loaded");
 
+	// Variables
+	var num1 = document.querySelector("#input1");
+	var num2 = document.querySelector("#input2");
+	var equals = document.querySelector("#trigger");
+	var outcome = document.querySelector("#value");
+	// console.log(num1);
+	// console.log(num2);
+	// console.log(equals);
+	// console.log(outcome);
+	// (these console bits are to test that the js is working in the browser)
+	
+	
+	// Functions
+	function addStuff(){
+		// console.log("from addStuff");
+	}
+	
+	
+	// Listeners
+      equals.addEventListener("click", addStuff, false); 
+      // the function is "addstuff" 
+
 }) ();
